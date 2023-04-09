3675402522a2daa65016fc30c376b926dd1f95bd
duywin <128769273+duywin@users.noreply.github.com>
2023-04-09 21:16:16 +0700

    Mydatabase: Thêm sửa xóa các bảng
    Đây là lệnh xóa
        public long xoa(SinhVien sinhVien) {
            /*
             * Tương tự cho xóa một dòng dữ liệu cần
             * 3 đối số là tên bảng, câu điều kiện và
             * cuối cùng là đối số cho câu điều kiện
             * nhưng có thể gộp 2 đối số làm 1 như ở
             * dưới.
             */
            return database.delete(DBHelper .TEN_BANG_SINHVIEN, DBHelper
                    .COT_TEN + " = " + "'" +
                    sinhVien.get_ten() + "'", null);
        }

---
+++ app/src/main/java/edu/huflit/learningenglishapp/Mydatabase.java
@@ -0,0 +1,77 @@
+package edu.huflit.learningenglishapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class Mydatabase {
+    SQLiteDatabase database;
+    DBHelper helper;
+    public Mydatabase(Context context) {
+    DBHelper helper = new DBHelper(context);
+        database = helper.getWritableDatabase();
+    }
+    public Cursor layTatCaDuLieu() {
+        String[] cot = {
+        DBHelper.COT_HEADLINE, DBHelper.COT_IDBLOG, DBHelper.COT_IDUSER, DBHelper.COT_BLOGWRITING,
+        DBHelper.COT_TRIES, DBHelper.COT_IDTEST, DBHelper.COT_SCORE, DBHelper.COT_CHOICE,
+        DBHelper.COT_DETAILS, DBHelper.COT_IDQUESTION, DBHelper.COT_DESCRIPTION,
+        DBHelper.COT_AMOUNT,DBHelper.COT_LEVEL,DBHelper.COT_AGE,DBHelper.COT_GENDER,
+        DBHelper.COT_PASSWORD,DBHelper.COT_ROLEUSER,DBHelper.COT_USERNAME
+        };
+        //lay het cot trong database
+        Cursor cursor = null;
+        cursor = database.query(DBHelper.
+                        TEN_BANG_User, cot, null, null, null, null,
+                DBHelper.COT_IDUSER + " DESC");
+        cursor = database.query(DBHelper.
+                        TEN_BANG_Test, cot, null, null, null, null,
+                DBHelper.COT_IDTEST + " DESC");
+        cursor = database.query(DBHelper.
+                        TEN_BANG_Question, cot, null, null, null, null,
+                DBHelper.COT_IDQUESTION + " DESC");
+        cursor = database.query(DBHelper.
+                        TEN_BANG_Exam, cot, null, null, null, null,
+                DBHelper.COT_IDTEST + " DESC");
+        cursor = database.query(DBHelper.
+                        TEN_BANG_Result, cot, null, null, null, null,
+                DBHelper.COT_IDUSER + " DESC");
+        cursor = database.query(DBHelper.
+                        TAO_BANG_Blog, cot, null, null, null, null,
+                DBHelper.COT_IDBLOG + " DESC");
+        return cursor;
+    }
+    //Thêm + sửa bảng user (không có xóa vì chưa có tính năng xóa tài khoản
+    public long them(User user) {
+        ContentValues values = new ContentValues();
+        values.put(DBHelper.COT_USERNAME,
+                user.get_username());
+        values.put(DBHelper.COT_PASSWORD,
+                user.get_password());
+        values.put(DBHelper.COT_AGE,
+                user.get_age());
+        values.put(DBHelper.COT_GENDER,
+                user.get_gender());
+        values.put(DBHelper.COT_LEVEL,
+                user.get_level());
+        values.put(DBHelper.COT_ROLEUSER,
+                user.get_roleuser());
+        return database.insert(DBHelper.
+                TEN_BANG_User, null, values);
+    }
+    public long sua(User user) {
+        ContentValues values = new ContentValues();
+        values.put(DBHelper.COT_USERNAME,
+                user.get_username());
+        values.put(DBHelper.COT_PASSWORD,
+                user.get_password());
+        values.put(DBHelper.COT_AGE,
+                user.get_age());
+        values.put(DBHelper.COT_GENDER,
+                user.get_gender());
+        return database.update(DBHelper.TEN_BANG_User, values,
+                DBHelper.COT_IDUSER + " = " + user.get_IDUser(), null);
+    }
+
+}
