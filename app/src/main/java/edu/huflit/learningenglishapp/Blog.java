bf798848c5e8e8cf03dd8022417ffc207663ad7e
duywin <128769273+duywin@users.noreply.github.com>
2023-04-09 13:57:57 +0700

    Table blog
    /* Bảng để lưu các blod trong chuyên mục cộng đồng để phòng mất dữ liệu */

---
+++ app/src/main/java/edu/huflit/learningenglishapp/Blog.java
@@ -0,0 +1,39 @@
+package edu.huflit.learningenglishapp;
+
+public class Blog {
+    private long _IDblog;
+    private String _headline,_blogwriting;
+
+    public Blog() {
+    }
+
+    public Blog(long _IDblog, String _headline, String _blogwriting) {
+        this._IDblog = _IDblog;
+        this._headline = _headline;
+        this._blogwriting = _blogwriting;
+    }
+
+    public long get_IDblog() {
+        return _IDblog;
+    }
+
+    public void set_IDblog(long _IDblog) {
+        this._IDblog = _IDblog;
+    }
+
+    public String get_headline() {
+        return _headline;
+    }
+
+    public void set_headline(String _headline) {
+        this._headline = _headline;
+    }
+
+    public String get_blogwriting() {
+        return _blogwriting;
+    }
+
+    public void set_blogwriting(String _blogwriting) {
+        this._blogwriting = _blogwriting;
+    }
+}
