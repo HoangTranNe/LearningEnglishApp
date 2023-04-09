784168462e3414dd5cb3ee77409de91ecf82fd86
duywin <128769273+duywin@users.noreply.github.com>
2023-04-09 13:47:28 +0700

    Table Question
    /* Bảng để tạo câu hỏi và lưu đáp, score có 0 cho đáp án sai và 1 cho đáp án đúng */

---
+++ app/src/main/java/edu/huflit/learningenglishapp/Question.java
@@ -0,0 +1,46 @@
+package edu.huflit.learningenglishapp;
+
+public class Question {
+    private long _Idquestion;
+    private String _details,_choice;
+    private int _score;
+
+    public Question(long _Idquestion, String _details, String _choice, int _score) {
+        this._Idquestion = _Idquestion;
+        this._details = _details;
+        this._choice = _choice;
+        this._score = _score;
+    }
+
+    public long get_Idquestion() {
+        return _Idquestion;
+    }
+
+    public void set_Idquestion(long _Idquestion) {
+        this._Idquestion = _Idquestion;
+    }
+
+    public String get_details() {
+        return _details;
+    }
+
+    public void set_details(String _details) {
+        this._details = _details;
+    }
+
+    public String get_choice() {
+        return _choice;
+    }
+
+    public void set_choice(String _choice) {
+        this._choice = _choice;
+    }
+
+    public int get_score() {
+        return _score;
+    }
+
+    public void set_score(int _score) {
+        this._score = _score;
+    }
+}
