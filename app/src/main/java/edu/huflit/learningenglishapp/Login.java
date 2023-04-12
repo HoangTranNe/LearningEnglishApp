6ec57aa4ad65c37451e1a78513d8c2ce2c77b3fb
duywin <128769273+duywin@users.noreply.github.com>
2023-04-12 21:47:04 +0700

    Back end cho trang đăng nhập

---
+++ app/src/main/java/edu/huflit/learningenglishapp/Login.java
@@ -0,0 +1,22 @@
+package com.example.learningenglishapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    EditText medtname, medtpass;
+    Button bttlogin, bttresigter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        medtname = findViewById(R.id.EdtName);
+        medtpass = findViewById(R.id.EdtPass);
+        bttlogin = findViewById(R.id.BttLogin);
+        bttresigter = findViewById(R.id.BttResig);
+    }
+}
