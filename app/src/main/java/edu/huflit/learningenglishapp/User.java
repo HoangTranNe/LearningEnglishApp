1c15ef55191adf8969539b2edb04eaa8b54905ea
duywin <128769273+duywin@users.noreply.github.com>
2023-04-13 10:29:42 +0700

    Update User.java

--- app/src/main/java/edu/huflit/learningenglishapp/User.java
+++ app/src/main/java/edu/huflit/learningenglishapp/User.java
@@ -1,4 +1,14 @@
-package edu.huflit.learningenglishapp;
+package com.example.learningenglishapp;
 
-public class User {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+    }
 }
