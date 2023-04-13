eb26dc0814ccfbe608ff5a361b26085e39fe6650
HoangTranNe <tranbaohoang70@gmail.com>
2023-04-13 11:06:57 +0700

    thêm chức năng bài blog

--- app/src/main/java/edu/huflit/learningenglishapp/MainActivity.java
+++ app/src/main/java/edu/huflit/learningenglishapp/MainActivity.java
@@ -2,17 +2,33 @@ package edu.huflit.learningenglishapp;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import Database.User;
 import edu.huflit.learningenglishapp.account.Login;
+import edu.huflit.learningenglishapp.main_layout.Blog;
 import edu.huflit.learningenglishapp.main_layout.History_Library;
 import edu.huflit.learningenglishapp.main_layout.Review;
 
 
 public class MainActivity extends AppCompatActivity{
+
+    private EditText inpBlog;
+    private Button postBlog;
+    private RecyclerView showBlog;
+    private Blog.BlogAdapter blogAdapter;
+    private List<Blog> blogList = new ArrayList<>();
 
     ImageButton back, user,history, review;
 
@@ -48,6 +64,29 @@ public class MainActivity extends AppCompatActivity{
             startActivity(intent);
             finish();
         });
+
+        inpBlog = findViewById(R.id.inpBlog);
+        postBlog = findViewById(R.id.postBlog);
+        showBlog = findViewById(R.id.showBlog);
+
+        blogAdapter = new Blog.BlogAdapter(blogList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        showBlog.setLayoutManager(layoutManager);
+        showBlog.setAdapter(blogAdapter);
+
+        postBlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = "Title";
+                String content = inpBlog.getText().toString();
+
+                Blog blog = new Blog(title, content);
+                blogList.add(blog);
+                blogAdapter.notifyDataSetChanged();
+                inpBlog.setText("");
+            }
+        });
+
 
     }
 
--- app/src/main/java/edu/huflit/learningenglishapp/main_layout/Blog.java
+++ app/src/main/java/edu/huflit/learningenglishapp/main_layout/Blog.java
@@ -1,14 +1,23 @@
 package edu.huflit.learningenglishapp.main_layout;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+import edu.huflit.learningenglishapp.R;
+
 public class Blog {
     private String title;
     private String content;
-    private String date;
 
-    public Blog(String title, String content, String date) {
+    public Blog(String title, String content) {
         this.title = title;
         this.content = content;
-        this.date = date;
     }
 
     public String getTitle() {
@@ -26,13 +35,43 @@ public class Blog {
     public void setContent(String content) {
         this.content = content;
     }
+    public static class BlogAdapter extends RecyclerView.Adapter<BlogAdapter.ViewHolder> {
+        private List<Blog> blogs;
 
-    public String getDate() {
-        return date;
+        public BlogAdapter(List<Blog> blogs) {
+            this.blogs = blogs;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_blog, parent, false);
+            return new ViewHolder(view);
+        }
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Blog blog = blogs.get(position);
+            holder.titleTextView.setText(blog.getTitle());
+            holder.contentTextView.setText(blog.getContent());
+        }
+        @Override
+        public int getItemCount() {
+            return blogs.size();
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView titleTextView;
+            public TextView contentTextView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                titleTextView = itemView.findViewById(R.id.TwTitle);
+                contentTextView = itemView.findViewById(R.id.tvContent);
+            }
+        }
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
 }
+
 
